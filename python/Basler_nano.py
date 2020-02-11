2b62c6a7824942feca6fed673fa4604f7c89bb2d
Paseul <machianb@naver.com>
2020-02-11 15:29:16 +0900

    Create Basler_nano.py
    remove video save part and reduce img size for Jetson nano

---
+++ python/Basler_nano.py
@@ -0,0 +1,68 @@
+'''
+A simple Program for grabing video from basler camera and converting it to opencv img.
+Tested on Basler acA1300-200uc (USB3, linux 64bit , python 3.5)
+
+'''
+from pypylon import pylon
+import numpy as np
+import cupy as cp
+from matplotlib import pyplot as plt
+import cv2
+
+# conecting to the first available camera
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+# Grabing Continusely (video) with minimal delay
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+converter = pylon.ImageFormatConverter()
+
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)   
+
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = converter.Convert(grabResult)
+        img = image.GetArray()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (960, 687))    
+
+        np.where(img < 5, 0, img)  
+
+        ret, thresh = cv2.threshold(img, 200, 255, 0)
+
+        M = cv2.moments(thresh)
+        if (M["m00"]!= 0):
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cX2 = int(M["mu20"] / M["m00"])
+            cXY = int(M["mu11"] / M["m00"])
+            cY2 = int(M["mu02"] / M["m00"])
+
+            dX = int(2*(2**0.5)*((cX2 + cY2) + 2*abs(cXY))**0.5)
+            dY = int(2*(2**0.5)*((cX2 + cY2) - 2*abs(cXY))**0.5)
+            if((cX2 - cY2)!=0):
+                t = 2 * cXY / (cX2 - cY2)
+            else:
+                t = 0
+            
+            theta = 0.5 * np.arctan(t) * 180
+
+            cv2.ellipse(img, (cX, cY), (dX, dY), theta, 0, 360, (255, 255, 255))
+
+        cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+        cv2.imshow('title', img)
+
+        k = cv2.waitKey(1)           
+
+        if k == 27:
+            break
+
+    grabResult.Release()   
+    
+# Releasing the resource    
+camera.StopGrabbing()
+cv2.destroyAllWindows()
