36ef80c7fe1732464687edb6f082f9391edfdbdc
Paseul <machianb@naver.com>
2020-11-16 14:01:30 +0900

    python update

--- python/Basler_fourier.py
+++ python/Basler_fourier.py
@@ -5,10 +5,7 @@ Tested on Basler acA1300-200uc (USB3, linux 64bit , python 3.5)
 '''
 from pypylon import pylon
 import numpy as np
-import cupy as cp
-from matplotlib import pyplot as plt
 import cv2
-import skimage as sk
 
 # save video
 videoWriter = cv2.VideoWriter()
@@ -34,39 +31,17 @@ while camera.IsGrabbing():
         # Access the image data
         image = converter.Convert(grabResult)
         img = image.GetArray()
-        img = cv2.resize(img, (1920, 1374))        
+        img = cv2.resize(img, (512, 512))      
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-        dft = cv2.dft(cp.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
-        dft_shift = cp.fft.fftshift(dft)
-        
-        magnitude_spectrum = 20*cp.log(cp.array(cv2.magnitude(cp.asnumpy(dft_shift[:,:,0]),cp.asnumpy(dft_shift[:,:,1]))))
-
-        rows, cols = img.shape
-        crow,ccol = int(rows/2), int(cols/2)        
-        
-        mask = cp.zeros((rows,cols,2),cp.uint8)      # create a mask first, center square is 1, remaining all zeros
-        mask[crow-30:crow+30, ccol-30:ccol+30] = 1
-        
-        fshift = dft_shift*mask            # apply mask and inverse DFT
-        f_ishift = cp.fft.ifftshift(fshift)        
-        
-        fshift = dft_shift*mask               # apply mask and inverse DFT
-        f_ishift = cp.fft.ifftshift(fshift)
-        img_back = cv2.idft(cp.asnumpy(f_ishift))
-        img_back = cv2.magnitude(img_back[:,:,0],img_back[:,:,1])        
-        
-        img_back = cp.array(cp.array(img_back),dtype='float32')/float(2**32-1)
-        img_back = cp.asnumpy(img_back)
-
-        minVal = np.amin(img_back)
-        maxVal = np.amax(img_back)
-        img_back = cv2.convertScaleAbs(img_back, alpha=255.0/(maxVal - minVal), beta=-minVal * 255.0/(maxVal - minVal))
+        fft = np.fft.fft2(img)
+        fftShift = np.fft.fftshift(fft)
+        magnitude =  0.1 * np.log(np.abs(fftShift))
         
         cv2.namedWindow('title', cv2.WINDOW_NORMAL)
-        cv2.imshow('title', img_back)
+        cv2.imshow('title', magnitude)
 
-        videoWriter.write(img_back)
+        # videoWriter.write(magnitude)
 
         k = cv2.waitKey(1)           
 
@@ -76,5 +51,5 @@ while camera.IsGrabbing():
     
 # Releasing the resource    
 camera.StopGrabbing()
-videoWriter.release()
+# videoWriter.release()
 cv2.destroyAllWindows()
--- python/video_fft.py
+++ python/video_fft.py
@@ -1,3 +1,5 @@
+# python video_tracking.py --v test_500us.avi
+
 # import the necessary packages
 from collections import deque
 from imutils.video import VideoStream
@@ -33,10 +35,10 @@ else:
 time.sleep(2.0)
 
 # save video
-videoWriter = cv2.VideoWriter()
+# videoWriter = cv2.VideoWriter()
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-videoWriter.open(args["output"], fourcc, 14, (3840, 2748), False)   
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# videoWriter.open(args["output"], fourcc, 14, (3840, 2748), False)   
 
 # keep looping
 while True:
@@ -48,25 +50,31 @@ while True:
 	# then we have reached the end of the video
 	if frame is None:
 		break
-
+	
 	img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	img = cv2.resize(img, (512, 512))
+	# img = img/255
+	fft = np.fft.fft2(img)
+	fftShift = np.fft.fftshift(fft)
 
-	np.where(img < 5, 0, img)  
+	magnitude = 0.1 * np.log(np.abs(fftShift))
 
-	img = Iso11146.ellipse(img, args["minimum_thresh"], args["maximum_thresh"])
+	# np.where(img < 5, 0, img)  
+
+	# img = Iso11146.ellipse(img, args["minimum_thresh"], args["maximum_thresh"])
 
 	cv2.namedWindow('title', cv2.WINDOW_NORMAL)
-	cv2.imshow('title', img)
+	cv2.imshow('title', magnitude)
 
-	videoWriter.write(img)
+	# videoWriter.write(img)
 	
 	k = cv2.waitKey(1) 
-	# time.sleep(0.1)          	
+	# time.sleep(10)          	
 	
 	if k == 27:
 		break
 
 vs.release()
-videoWriter.release()
+# videoWriter.release()
 # close all windows
 cv2.destroyAllWindows()
